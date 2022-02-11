5ebb83f3ba1ab856f324a096a7caf35384cc56b4
Thales Xavier de Almeida Nogueira <thales.xavier@totvs.com.br>
2022-02-11 19:48:26 -0300

    Primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const URLGlb = 'http://localhost:3001/remoteEntry.js';
+const URLEdu = 'http://localhost:3002/remoteEntry.js';
+
+export const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'globais',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: URLGlb,
+        exposedModule: './Module',
+      }).then((m) => m.InteropModule),
+  },
+  {
+    path: 'educacional',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: URLEdu,
+        exposedModule: './Module',
+      }).then((m) => m.InteropModule),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
