81aa036e826b3e49b1b17ab058cb9a9e7d5779a6
Thales Xavier de Almeida Nogueira <thales.xavier@totvs.com.br>
2022-02-14 09:25:11 -0300

    MultiRepo Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-  public title: string = 'MultiRePo Home Component';
+  public title: string = 'MultiRepo Home Component';
   ngOnInit() {
   }
 
