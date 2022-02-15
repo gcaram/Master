b457ec6d457cd4832b8e883f5c5f37b39a04c5b1
Thales Xavier de Almeida Nogueira <thales.xavier@totvs.com.br>
2022-02-15 07:50:02 -0300

    remoteEntry.js

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { loadRemoteEntry } from '@angular-architects/module-federation';
 
 Promise.all([
    loadRemoteEntry(
-      { type: 'module', remoteEntry: environment.baseUrlGlobais + '/globais.js' }
+      { type: 'module', remoteEntry: environment.baseUrlGlobais + '/remoteEntry.js' }
    ),
    loadRemoteEntry(
-      { type: 'module', remoteEntry: environment.baseUrlEdu + '/educacional.js' }
+      { type: 'module', remoteEntry: environment.baseUrlEdu + '/remoteEntry.js' }
    )
 ])
    .catch(err => console.error('Error loading remote entries', err))
--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,8 @@ module.exports = {
         library: { type: "module" },
 
          remotes: {
-             "globais": "https://icy-rock-015a79e10.1.azurestaticapps.net/globais.js",
-             "educacional": "https://proud-river-070d7d110.1.azurestaticapps.net/educacional.js",
+             "globais": "https://icy-rock-015a79e10.1.azurestaticapps.net/remoteEntry",
+             "educacional": "https://proud-river-070d7d110.1.azurestaticapps.net/remoteEntry.js",
         },
 
         shared: share({
